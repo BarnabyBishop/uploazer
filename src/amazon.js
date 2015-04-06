6b66de23788e2ff7b8343d5b0267a0da1620db69
Barnaby Bishop <barneyb@gmail.com>
2015-04-06 19:21:35 +1000

    Added logic to check if ll files ave thumbnails.

--- src/amazon.js
+++ src/amazon.js
@@ -1,47 +1,72 @@
+const _ = require('lodash');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const Promise = require('bluebird');
+
 const listLimit = 1000;
+const thumbBucket = 'Thumbnails/';
+
 Promise.promisifyAll(s3);
 
-
 class Amazon {
-  constructor(bucket, prefix) {
-    this.bucket = bucket;
-    this.prefix = prefix;
-    this.content = null;
-    this.thumbnails = null;
-  }
+	constructor(bucket, prefix) {
+		this.bucket = bucket;
+		this.prefix = prefix;
+		this.content = {};
+		this.thumbnails = {};
+	}
 
-  async checkThumbnails() {
-    // Load all objects in current bucket.
-    this.content = await this.loadAllObjects();
-    this.thumbnails = await this.loadAllObjects({ prefix: 'Thumbnails/' + this.prefix });
-  }
+	async checkThumbnails() {
+		// Load all objects in current bucket.
+		this.content = await this.loadAllObjects({});
+		this.thumbnails = await this.loadAllObjects({ prefix: thumbBucket + this.prefix });
+		console.log(`Loaded ${this.content.length} items and ${this.thumbnails.length} thumbnails.`);
+		const missingThumbnails = this.findMissingThumbnails();
+		console.log(`Found ${missingThumbnails.length} missing thumbnails.`);
+	}
 
-  async loadAllObjects({ marker: marker, prefix: prefix }) {
-    let objects = [];
-    objects = objects.concat(await this.listObjects({ marker: marker, prefix: prefix }));
+	//=================
+	// S3 load methods
 
-    if (count == listLimit) {
-        let marker = objects[objects.length - 1].Key;
-        objects = objects.concat(await this.loadAllObjects({ marker: marker, prefix: prefix }));
-    }
-    return objects;
-  }
+	async loadAllObjects({ marker: marker, prefix: prefix }) {
+		let objects = await this.listObjects({ marker: marker, prefix: prefix });
 
-  listObjects({ marker: marker, prefix: prefix }) {
-    let params = { Bucket: this.bucket };
-    if (marker) {
-      params.Marker = marker;
-    }
-    params.Prefix = prefix ? prefix : this.prefix;
+		if (objects.length == listLimit) {
+				let marker = objects[objects.length - 1].Key;
+				objects = objects.concat(await this.loadAllObjects({ marker: marker, prefix: prefix }));
+		}
+		return objects;
+	}
 
-    return s3.listObjectsAsync(params).then((data) => {
-      this.content = this.content.concat(data.Contents);
-      return data.Contents;
-    });
-  }
+	listObjects({ marker: marker, prefix: prefix }) {
+		let params = { Bucket: this.bucket };
+		if (marker) {
+			params.Marker = marker;
+		}
+		params.Prefix = prefix ? prefix : this.prefix;
+
+		return s3.listObjectsAsync(params).then((data) => {
+			return data.Contents;
+		}).error((error) => {
+			console.error(error);
+		});
+	}
+
+	//===================
+	// Thumbnail methods
+	findMissingThumbnails() {
+		let missingThumbnails = [];
+		_.forEach(this.content, (item) => {
+			let thumbnail = _.find(this.thumbnails, (thumbnail) => {
+					return thumbnail.Key.replace(thumbBucket, '') === item.Key;
+				}
+			);
+			if (!thumbnail) {
+				missingThumbnails.push(item.Key);
+			}
+		});
+		return missingThumbnails;
+	}
 }
 
 
--- src/app.js
+++ src/app.js
@@ -1,28 +1,6 @@
 const babel = require("babel/register")({ experimental: true });
 const Amazon = require('./amazon');
 
-const amazon = new Amazon('barney-photos', 'Honeymoon');
-//const amazon = new Amazon('barney-photos', 'BB Android Phone 2');
+// const amazon = new Amazon('barney-photos', 'Honeymoon');
+const amazon = new Amazon('barney-photos', 'BB Android Phone 2');
 amazon.checkThumbnails();
-
-/*
-s3.listObjects({ Bucket: 'barney-photos' }, (error, data) => {
-	if (error) {
-    	console.log(error); // error is Response.error
-  	} else {
-  		let contents = data.Contents;
-    	console.log(`${contents.length} items.`);
-    	console.log(contents[0]);
-    	console.log(contents[contents.length - 1]);
-		let marker = contents[contents.length - 1].Key;
-    	s3.listObjects({ Bucket: 'barney-photos', Marker: marker }, (error, data) => {
-    		let contents = data.Contents;
-    		console.log(`${contents.length} items.`);
-    		console.log(contents[0]);
-    		console.log(contents[contents.length - 1]);
-
-    	});
-
-  	}
-});
-*/
