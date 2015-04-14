77c6f8237dd51cab238ae0dbc2b82ab8a8558094
Barnaby Bishop <barneyb@gmail.com>
2015-04-15 08:11:29 +1000

    Missing files are now uploaded to S3 with progress bar.

--- src/amazon.js
+++ src/amazon.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const _ = require('lodash');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const Promise = require('bluebird');
+const ProgressBar = require('progress');
 
 const listLimit = 1000;
 const thumbBucket = 'Thumbnails/';
@@ -9,20 +11,103 @@ const thumbBucket = 'Thumbnails/';
 Promise.promisifyAll(s3);
 
 class Amazon {
-	constructor(bucket, prefix) {
+	constructor(bucket, prefix, filePath) {
 		this.bucket = bucket;
 		this.prefix = prefix;
-		this.content = {};
+		this.filePath = filePath;
+
+		this.files = [];
+		this.bucketFiles = {};
 		this.thumbnails = {};
+
+		this.missingFiles = [];
+
 	}
 
 	async checkThumbnails() {
-		// Load all objects in current bucket.
-		this.content = await this.loadAllObjects({});
+		// Load a list of filenames from the current directory.
+		this.getFileList();
+		// Load all objects + thumbnails in current bucket.
+		this.bucketFiles = await this.loadAllObjects({});
+		// Find what files need to be uploaded
+		const missingFilesCount = this.findMissingFiles();
+		console.log(`${missingFilesCount} missing files.`);
+
+		if (missingFilesCount > 0) {
+			await this.uploadMissingFiles();
+		}
+
 		this.thumbnails = await this.loadAllObjects({ prefix: thumbBucket + this.prefix });
-		console.log(`Loaded ${this.content.length} items and ${this.thumbnails.length} thumbnails.`);
+		console.log(`Loaded ${this.bucketFiles.length} items and ${this.thumbnails.length} thumbnails.`);
+
+
+		// Find what thumbnails need to be uploaded
 		const missingThumbnails = this.findMissingThumbnails();
-		console.log(`Found ${missingThumbnails.length} missing thumbnails.`);
+		console.log(`${missingThumbnails.length} missing thumbnails.`);
+	}
+
+	//=================
+	// File methods
+
+	getFileList(dir = '') {
+		let fullPath = this.filePath + dir;
+		let fullBucketPath = this.prefix + '/' + dir;
+		let files = fs.readdirSync(fullPath);
+		for (let i = 0; i < files.length; i++) {
+			let stats = fs.statSync(fullPath + files[i]);
+			if (stats.isFile()) {
+				this.files.push({ path: fullPath, name: files[i], bucketAlias: fullBucketPath });
+			}
+			else if (stats.isDirectory()) {
+				this.getFileList(`${dir}${files[i]}/`);
+			}
+		}
+	}
+
+	findMissingFiles() {
+		this.missingFiles = [];
+		for (let file of this.files) {
+			if (!_.findWhere(this.bucketFiles, { Key: file.bucketAlias + file.name })) {
+				this.missingFiles.push(file);
+			}
+		}
+		return this.missingFiles.length;
+	}
+
+	async uploadMissingFiles() {
+		let loadingFile = false;
+		for (let file of this.missingFiles) {
+			let filename = file.name;
+			let bucket = this.bucket + file.bucketAlias;
+			if (!loadingFile) {
+				loadingFile = true;
+				console.log(`Uploading ${file.path + filename} to ${file.bucketAlias + filename}`)
+				let fileBuffer = fs.readFileSync(file.path + filename);
+				var params = {Bucket: this.bucket, Key: file.bucketAlias + filename, ContentType: 'image/JPEG', Body: fileBuffer };
+				await this.putObject(params);
+			}
+		}
+	}
+
+	putObject(params) {
+		let uploadProgress = 0;
+		let bar = new ProgressBar(':bar:percent', { total: 100, width: 20 });
+		return s3.putObject(params)
+				.on('httpUploadProgress', function (progress) {
+					let percentage = parseInt(progress.loaded / progress.total * 100)
+					if (percentage > uploadProgress) {
+						uploadProgress = percentage;
+						bar.tick();
+					}
+				})
+				.send()
+				.then((err, data) => {
+					if (err) {
+						console.log('Error: ', err);
+					} else {
+						console.log(`Uploaded! ${file.path + filename} to ${file.bucketAlias + filename}`);
+					}
+			  });
 	}
 
 	//=================
@@ -56,7 +141,7 @@ class Amazon {
 	// Thumbnail methods
 	findMissingThumbnails() {
 		let missingThumbnails = [];
-		_.forEach(this.content, (item) => {
+		_.forEach(this.bucketFiles, (item) => {
 			let thumbnail = _.find(this.thumbnails, (thumbnail) => {
 					return thumbnail.Key.replace(thumbBucket, '') === item.Key;
 				}
--- src/app.js
+++ src/app.js
@@ -2,5 +2,7 @@ const babel = require("babel/register")({ experimental: true });
 const Amazon = require('./amazon');
 
 // const amazon = new Amazon('barney-photos', 'Honeymoon');
-const amazon = new Amazon('barney-photos', 'BB Android Phone 2');
+const amazon = new Amazon('barney-photos',
+                          'BB Android Phone 2',
+                          'C:\\Users\\B\\Pictures\\BB Android Phone 2\\');
 amazon.checkThumbnails();
