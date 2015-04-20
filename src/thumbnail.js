8e1f4877e5d9f8cab88b1273a4d5d72c14bae5cb
Barnaby <barnaby.bishop@mi9.com.au>
2015-04-20 17:30:18 +1000

    Removed old unused file. Fleshed out thumbnail module.

--- src/thumbnail.js
+++ src/thumbnail.js
@@ -1,9 +1,29 @@
-const gm = require('gm');
+const fs = require('fs');
 const exec = require('child_process').exec;
+const gm = require('gm');
+const mime = require('mime');
 
-module.exports = {
+const imageTypes = [
+    'image/jpg',
+    'image/gif'
+];
+
+const videoTypes = [
+    'video/mp4'
+];
+
+let thumbnail = module.exports = {
     get: function(filePath, size, time, callback) {
-
+        const mimeType = mime.lookup(filePath);
+        if (imageTypes.indexOf(mimeType) > -1) {
+            thumbnail.getImageThumbnail(filePath, size, callback);
+        }
+        else if (videoTypes.indexOf(mimeType) > -1) {
+            thumbnail.getVideoThumbnail(filePath, size, time, callback);
+        }
+        else {
+            callback();
+        }
     },
 
     getImageThumbnail: function(filePath, size, callback) {
@@ -21,9 +41,12 @@ module.exports = {
 
     getVideoThumbnail: function(filePath, size, time, callback) {
         exec('ffmpeg -ss ' + time + ' -vframes 1 -i ' + filePath + ' -y -s ' + size.height + ' -f image2 tmp/thumb1.jpg', () => {
-            if (callback) {
-                return callback();
-            }
+            fs.readFile('tmp/thumb1.jpg', (err, buffer) => {
+                if (err) {
+                    throw err;
+                }
+                callback(buffer);
+            });
         });
     }
 };
